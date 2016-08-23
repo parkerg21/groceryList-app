3e449d57c4e31a561b03f1f7fba343a582879a51
parkerg21 <parkerg21@gmail.com>
2016-08-23 11:22:20 -0400

    Add product component

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -9,6 +9,7 @@ var Angular2App = require('angular-cli/lib/broccoli/angular2-app');
 module.exports = function(defaults) {
   return new Angular2App(defaults, {
     vendorNpmFiles: [
+      'ng2-bootstrap/**/*.js',
       'systemjs/dist/system-polyfills.js',
       'systemjs/dist/system.src.js',
       'zone.js/dist/**/*.+(js|js.map)',
