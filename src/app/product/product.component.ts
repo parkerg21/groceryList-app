3709a62003bad257cf0ad65188d09c9cb75bdefa
parkerg21 <parkerg21@gmail.com>
2016-08-23 11:28:59 -0400

    Add product component

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-product',
+  templateUrl: 'product.component.html',
+  styleUrls: ['product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
