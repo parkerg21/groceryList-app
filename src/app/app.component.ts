564f4b0b32e69906b367cf3d52c4947ccc3c2388
parkerg21 <parkerg21@gmail.com>
2016-08-26 20:29:13 -0400

    Add Product Interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import {ProductComponent} from "./product/product.component";
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [ProductComponent]
 })
 export class AppComponent {
-  title = 'app works!';
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+
 
 @Component({
   moduleId: module.id,
@@ -7,7 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['product.component.css']
 })
 export class ProductComponent implements OnInit {
-
+  pageTitle: string = 'Product List';
+  productFilter: string = 'cart';
+  products:  IProduct[] = [
+    {
+      "productId": 1,
+      "productName": "milk",
+      "price": 2.93,
+      "starRating": 4
+    },
+    {
+      "productId": 2,
+      "productName": "cereal",
+      "price": 3.33,
+      "starRating": 4
+    },
+    {
+      "productId": 3,
+      "productName": "lunchable",
+      "price": 1.50,
+      "starRating": 4
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
